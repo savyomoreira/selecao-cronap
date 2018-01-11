5d7b8f4d4ff350ba9c0dedefd7e4ffa59c07bf96
SAVYO <savyo_21@hotmail.com>
2018-01-11 03:31:08 -0300

    ajuste na edição de pessoa

--- src/main/resources/static/js/app/app.js
+++ src/main/resources/static/js/app/app.js
@@ -4,25 +4,3 @@ app.constant('urls', {
     BASE: 'http://localhost:8080/',
     PESSOA_SERVICE_API : 'http://localhost:8080/v1/pessoa/'
 });
-//
-////app.config(['$stateProvider', '$urlRouterProvider',
-////    function($stateProvider, $urlRouterProvider) {
-////
-////        $stateProvider
-////            .state('home', {
-////                url: '/',
-////                templateUrl: 'partials/list',
-////                controller:'UserController',
-////                controllerAs:'ctrl',
-////                resolve: {
-////                    users: function ($q, UserService) {
-////                        console.log('Load all users');
-////                        var deferred = $q.defer();
-////                        UserService.loadAllUsers().then(deferred.resolve, deferred.resolve);
-////                        return deferred.promise;
-////                    }
-////                }
-////            });
-////        $urlRouterProvider.otherwise('/');
-////    }]);
-//

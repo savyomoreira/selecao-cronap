0becaeeed1b0f1988fa5229808e937c7977ec187
Savyo Moreira <savyo_21@hotmail.com>
2018-01-11 12:35:28 -0200

    ajuste no contexto

--- src/main/java/com/smoreira/selecaocronapp/AppController.java
+++ src/main/java/com/smoreira/selecaocronapp/AppController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class AppController {
 
-    @RequestMapping("/home")
+    @RequestMapping("/*")
     public String home() {
         return "index";
     }
